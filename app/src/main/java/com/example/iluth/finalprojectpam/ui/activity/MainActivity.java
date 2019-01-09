943cf77777f1ddd594a2eea36a82a9484190d1ac
HarisAngriawan <39801304+16110339@users.noreply.github.com>
2019-01-09 22:59:20 +0700

    fix_notif

--- app/src/main/java/com/example/iluth/finalprojectpam/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/iluth/finalprojectpam/ui/activity/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.iluth.finalprojectpam.ui.activity;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -17,7 +17,7 @@ import com.example.iluth.finalprojectpam.ui.fragment.FavoriteFragment;
 import com.example.iluth.finalprojectpam.ui.fragment.MatchFragment;
 import com.example.iluth.finalprojectpam.ui.fragment.TeamFragment;
 
-import java.io.Serializable;
+
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
